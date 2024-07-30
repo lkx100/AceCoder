8284bcf1b34eb53f243723a0eefa694f4651e8d9
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-07-30 18:30:27 +0530

    Leaderboard-added, Homepage-updated

--- NameNotFound/dashboard/views.py
+++ NameNotFound/dashboard/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
 
-    return render(request, "home.html")
+    details = Codechef_database.objects.all().order_by('-latest_rating', 'latest_rank')
+    return render(request, "home.html", {'details': details})
 
 
 def dashboard(request):
