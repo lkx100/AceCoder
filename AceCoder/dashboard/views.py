805bc61f96e49c18c06eb469dbf0467debcd926a
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-08-04 11:37:45 +0530

    Download-feature-added

--- AceCoder/dashboard/views.py
+++ AceCoder/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import *
 from .Codechef import CodechefTools
+import pandas as pd
 
 # Create your views here.
 def home(request):
@@ -32,11 +34,14 @@ def fetch_details(request, codechef_id):
         stars = None
 
     if request.method == "POST":
+        download = request.POST.get('download', "False")
         plag = request.POST.get('plagarised', 'All')
         sortby = request.POST.get('sorting', 'None')
-        if contests and plag!='All':
+
+        if contests and plag != 'All':
             contests = [contest for contest in contests if (plag == 'Yes' and contest['penalised_in'] is not None) or (plag == 'No' and contest['penalised_in'] is None)]
-        if contests and sortby!='None':
+
+        if contests and sortby != 'None':
             if sortby == 'RatingInc':
                 contests.sort(key=lambda x: int(x['rating']))
             elif sortby == 'RatingDec':
@@ -45,21 +50,69 @@ def fetch_details(request, codechef_id):
                 contests.sort(key=lambda x: int(x['rank']))
             elif sortby == 'RankDec':
                 contests.sort(key=lambda x: int(x['rank']), reverse=True)
+
+        details = {
+            "codechef_id": codechef_id,
+            "contests": contests,
+            "num_of_contests": num_of_contests,
+            "num_of_plagarisms": num_of_plagarisms,
+            "num_of_problems": num_of_problems,
+            "stars": stars,
+            "all_contests": all_contests,
+            "plag": plag,
+            "sortby": sortby,
+        }
+
+        if download != 'False':
+            return download_details(details)
     else:
         plag = 'All'
         sortby = 'None'
-
-
-
-    details = {
-        "codechef_id": codechef_id, 
-        "contests": contests, 
-        "num_of_contests": num_of_contests, 
-        "num_of_plagarisms": num_of_plagarisms, 
-        "num_of_problems": num_of_problems, 
-        "stars": stars,
-        "all_contests": all_contests,
-        "plag": plag,
-        "sortby": sortby,
+        details = {
+            "codechef_id": codechef_id,
+            "contests": contests,
+            "num_of_contests": num_of_contests,
+            "num_of_plagarisms": num_of_plagarisms,
+            "num_of_problems": num_of_problems,
+            "stars": stars,
+            "all_contests": all_contests,
+            "plag": plag,
+            "sortby": sortby,
         }
+
     return render(request, "fetch_details.html", {'details': details})
+
+def download_details(details):
+    # Create the contests DataFrame
+    contests = details['contests']
+    contest_rows = []
+    for contest in contests:
+        row = {
+            'Contest': contest['name'],
+            'Rating': contest['rating'],
+            'Rank': contest['rank'],
+            'Plagarised': 'Yes' if contest['penalised_in'] else 'No'
+        }
+        contest_rows.append(row)
+    contests_df = pd.DataFrame(contest_rows)
+
+    # Create the general details DataFrame
+    general_details = {
+        'Codechef ID': [details['codechef_id']],
+        'Number of Contests': [details['num_of_contests']],
+        'Number of Plagarisms': [details['num_of_plagarisms']],
+        'Number of Problems Solved': [details['num_of_problems']],
+        'Stars': [details['stars']]
+    }
+    general_details_df = pd.DataFrame(general_details)
+
+    # Create a response object
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{details["codechef_id"]}_contest_details.csv"'
+
+    # Write both DataFrames to the CSV file
+    general_details_df.to_csv(response, index=False)
+    response.write("\n\n")  # Add some space between the two tables
+    contests_df.to_csv(response, index=False)
+
+    return response
