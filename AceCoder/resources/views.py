f120eee4aacdc408c0da178db0ad6c0ddd22ee46
lkx100 <lk5999950@gmail.com>
2024-08-18 00:29:57 +0530

    temp + added link from home page to blog_page via id

--- AceCoder/resources/views.py
+++ AceCoder/resources/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def resources_home(request):
@@ -6,5 +6,6 @@ def resources_home(request):
     return render(request, 'resources_home.html', context = {'blogs': blogs})
 
 def blog_page(request, blog_id):
-    blog = Post.objects.all()[blog_id-1]
+    # blog = Post.objects.all()[blog_id-1]    # Object by indexing
+    blog = get_object_or_404(Post, pk = blog_id)   # Object by id
     return render(request, 'blog_page.html', context = {'blog': blog})
