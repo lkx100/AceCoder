ff0d800f20c7d59a1bd16be05dce564909d5794a
SWESH1K <vagalamsweshikreddy@gmail.com>
2024-08-23 12:41:55 +0530

    Fixed layouts + Variablalised tags

--- AceCoder/resources/views.py
+++ AceCoder/resources/views.py
@@ -3,14 +3,19 @@ from .models import Post, PostTag, SubPost
 
 def resources_home(request):
     blogs = Post.objects.all()
-    return render(request, 'resources_home.html', context = {'blogs': blogs})
+    all_tags = PostTag.objects.all()
+    # for blog in blogs:
+    #     print(blog.tags.all())
+    return render(request, 'resources_home.html', context = {'blogs': blogs, 'all_tags': all_tags})
 
 def blog_page(request, blog_id):
     blog = get_object_or_404(Post, pk = blog_id)
+    all_tags = PostTag.objects.all()
     subposts = SubPost.objects.filter(parent_post = blog)
-    return render(request, 'blog_page.html', context = {'blog': blog, 'subposts': subposts})
+    return render(request, 'blog_page.html', context = {'blog': blog, 'subposts': subposts, "all_tags": all_tags})
 
 def tag_posts(request, tag):
+    all_tags = PostTag.objects.all()
     tag_obj = get_object_or_404(PostTag, post_tag=tag)
     blogs = Post.objects.filter(tags = tag_obj)
-    return render(request, 'tag_post.html', {"tag": tag, "blogs": blogs})
+    return render(request, 'tag_post.html', {"tag": tag, "blogs": blogs, "all_tags": all_tags})
