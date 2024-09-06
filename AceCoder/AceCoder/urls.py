abbbe4050a74544bae1738d6c47ad3400e21dc79
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-09-06 18:38:57 +0530

    admin bug fix - 3

--- AceCoder/AceCoder/urls.py
+++ AceCoder/AceCoder/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin', admin.site.urls),
     path('admin/', admin.site.urls),
     path('', include("dashboard.urls")),
     path('resources/', include("resources.urls")),
