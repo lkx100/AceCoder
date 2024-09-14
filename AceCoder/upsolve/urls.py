c530370ca0d9c5a06316f500e414466af0e354e8
lkx100 <lk5999950@gmail.com>
2024-09-14 12:35:54 +0530

    Added All Problem Set Table

--- AceCoder/upsolve/urls.py
+++ AceCoder/upsolve/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.upsolve_home, name="upsolve_home"),
+    path("problem_set/", views.problem_set, name="problem_set"),
     path("<str:contest_name>/", views.contest_page, name="contest_page"),
 ]
